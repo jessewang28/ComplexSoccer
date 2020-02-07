1a0db4e145e0acd09edb88d6f8559bf6c190882e
student <jessewang@billericak12.com>
2020-02-07 11:05:07 -0500

    Commit

--- src/com/company/Complex.java
+++ src/com/company/Complex.java
@@ -1,8 +1,42 @@
 package com.company;
 
 public class Complex {
+    private double x;
+    private double y;
 
-    public static void main(String[] args) {
-	// write your code here
+    public Complex(double a){
+        x=a;
+        y=0;
     }
+
+    public String toString(Complex z){
+        String c = x + " + " + y + "i";
+        return c;
+    }
+
+    public Complex(double a, double b){
+        x=a;
+        y=b;
+    }
+
+    public double abs(Complex c){
+        double squared = Math.pow(x, 2) + Math.pow(y,2);
+        double sqrt = Math.sqrt(squared);
+        return sqrt;
+    }
+
+    public Complex add(Complex other){
+        double first = this.x + other.x;
+        double second = this.y + other.y;
+        Complex z = new Complex (first, second);
+        return z;
+    }
+
+
+
+
+
+
+
 }
+
